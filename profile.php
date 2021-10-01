3bfbf3d0c0af3639ca7d87787e70d7611e97efcb
Danil Pedenko <66562869+Chooga777@users.noreply.github.com>
2021-10-01 09:36:44 +0300

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+class Start_from_main{
+    function backer(){
+        if (!$_SESSION['user']) {
+            header('Location: index.php');
+        }
+    }
+}
+$START = new Start_from_main;
+$START -> backer();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизація и реєстрація</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <!-- Профиль -->
+
+    <form>
+        <img src="<?= $_SESSION['user']['avatar'] ?>" style="border-radius: 50%; width: 200px;" alt="">
+        <?php
+        if (strlen($_SESSION['user']['avatar'])<=18) {
+            echo '<img style="border-radius: 50%; width: 200px;" src="img/anon.png" alt="">';
+        }
+        ?>
+        <h2 style="margin: 10px 0;"><?= $_SESSION['user']['full_name'] ?></h2>
+        <p>Пошта:<a href="#"> <?= $_SESSION['user']['email'] ?></a></p>
+        <p>Бажаєте повернутися на головну? - <a href="index.html">на головну</a>!</p>
+        <p><a href="vendor/logout.php" class="logout" style="margin: 5px 0 0 0;">Вихід</a><p>
+    </form>
+
+</body>
+</html>
